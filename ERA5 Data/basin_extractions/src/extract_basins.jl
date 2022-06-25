a8a3e6144e3e844b7f0722d85419644fd08617a9
Jesse <jesbrirob@gmail.com>
2022-06-24 17:11:12 -0800

    Extract Basins Code written

---
+++ ERA5 Data/basin_extractions/src/extract_basins.jl
@@ -0,0 +1,53 @@
+cd(@__DIR__)
+using Shapefile, DataFrames, CSV, NCDatasets, Dictionaries, PolygonOps, StaticArrays
+
+HUC6_path = "../data/HUC_Shapes/WBDHU6.shp"
+HUC8_path = "../data/HUC_Shapes/WBDHU8.shp"
+
+eratypes=["Base","Land"]
+eradirs = ["ERA5-SD-1979-2022-CREATE-2022-06-16.nc", "ERA5-Land-SD-1979-2022-DL-2022-6-15.nc"]
+era5dirs = "../../".*eratypes.*"/".*eradirs
+#Load in the netcdf data now
+datasets = Dictionary(eratypes, NCDataset.(era5dirs, "r"))
+lats =  getindex.(getindex.(datasets, "latitude"),:)
+lons =  getindex.(getindex.(datasets, "longitude"),:)
+lonlats = Dictionary(eratypes, [SVector.(lon, lat') for (lon,lat) in zip(lons,lats)])
+
+shapes = Dictionary([6,8],DataFrame.(Shapefile.Table.([HUC6_path, HUC8_path])))
+
+chena_basin_ids = ["19080306"]
+copper_ids = ["19020"] .* string.((1,2,3))
+kenai_ids = ["190203"]
+southeast_ids = ["190705"]
+remote_ids = ["19050301"]
+allowed_ids = [chena_basin_ids, copper_ids, kenai_ids, southeast_ids, remote_ids]
+basin_names = ["Chena", "Copper", "Kenai", "Southeast", "Remote"]
+
+#For each eratype, extract the indices of valid era5 points in the basin using a point in lat-lon polygon method
+for eratype in eratypes
+    for (basins, basinname) in zip(allowed_ids, basin_names)
+        allowed_points = SVector{2, Int}[]
+        for basin in basins
+            basinlen = length(basin)
+            basinshapes = shapes[basinlen]
+            #Now find the specific basin in the shapetable
+            basin_idx = findfirst( ==(basin), basinshapes[!, "huc$basinlen"])
+            #and use that to grab the polygon
+            my_polygon = basinshapes[basin_idx, :geometry]
+            #And extract the lat and lon of the polygon
+            poly_verts = SVector{2, Float32}.(getproperty.(my_polygon.points, :x), getproperty.(my_polygon.points, :y))
+            if poly_verts[end] ≠ poly_verts[begin]
+                push!(poly_verts, poly_verts[1])
+            end
+
+            #And now loop through every point and check if it's in the basin
+            erapoints = lonlats[eratype]
+            for era_point_idx in CartesianIndices(erapoints)
+                if inpolygon(erapoints[era_point_idx], poly_verts) == 1
+                    push!(allowed_points, SVector(Tuple(era_point_idx)...))
+                end
+            end
+        end
+        CSV.write("../$eratype/$(basinname)_era_points.csv", DataFrame(lonidx = first.(allowed_points), latidx = last.(allowed_points)))
+    end
+end
