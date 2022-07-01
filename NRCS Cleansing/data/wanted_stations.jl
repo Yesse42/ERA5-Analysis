9da3c4677ba1dd4b2f68b94aa4f8a9e42c6b5d39
Jesse <jesbrirob@gmail.com>
2022-06-30 17:31:21 -0800

    Rendered obsolete by better system

--- NRCS Cleansing/data/wanted_stations.jl
+++
@@ -1,7 +0,0 @@
-chena_basin_ids = ["19080306"]
-copper_ids = ["19020"] .* string.((1,2,3))
-kenai_ids = ["190203"]
-southeast_ids = ["190705"]
-remote_ids = ["19090101"]
-allowed_ids = [chena_basin_ids, copper_ids, kenai_ids, southeast_ids, remote_ids]
-basin_names = ["Chena", "Copper", "Kenai", "Southeast", "Remote"]
