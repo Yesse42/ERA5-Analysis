1e4c137ce239e398ff30f10ba930cbc170d1a154
Jesse <jesbrirob@gmail.com>
2022-07-20 13:32:13 -0800

    Update algorithm, refactor

--- Climatology/Snow_Off/src/snow_off.jl
+++ Climatology/Snow_Off/src/snow_off.jl
@@ -1,42 +1,11 @@
-using CSV, DataFrames, Dates, Dictionaries, JLD2, NCDatasets, StatsBase
+using CSV, DataFrames, Dates, Dictionaries, JLD2, NCDatasets, StatsBase, RecursiveArrayTools
 cd(@__DIR__)
 burrowactivate()
 import ERA5Analysis as ERA
 
 include("snow_off_single_year.jl")
 
-function snow_off(sd, times; min_snowy_days_before_snowpack_established)
-    nyears = -(year.((times[end], times[begin]))...) + 1
-    is_snow = sd .> is_snow_thresh
-    dates = Date.(times)
-    snowoff_arr = Array{Float64, 3}(undef, (size(sd, 1), size(sd, 2), nyears))
-    for i in axes(sd, 1), j in axes(sd, 2)
-        if ((i - 1) % 10 == 0) && (j == 1)
-            print(i)
-        end
-        if isnan(sd[i, j, 1])
-            snowoff_arr[i, j, :] .= NaN
-            continue
-        end
-        for t_idx in axes(snowoff_arr, 3)
-            current_year = year(times[begin]) + t_idx - 1
-            current_year_begin = only(searchsorted(dates, Date(current_year)))
-            current_year_end = searchsorted(dates, Date(current_year, 12, 31))
-            current_year_end = if isempty(current_year_end)
-                length(dates)
-            else
-                only(current_year_end)
-            end
-            t_idx_range = current_year_begin:current_year_end
-            snowoff_arr[i, j, t_idx] = snow_off_single_year(
-                @view(is_snow[i, j, t_idx_range]),
-                @view(dates[t_idx_range]);
-                min_snow = min_snowy_days_before_snowpack_established,
-            )
-        end
-    end
-    return snowoff_arr
-end
+na_or_miss(x) = ismissing(x) || isnan(x)
 
 using Plots
 pyplot()
@@ -52,41 +21,43 @@ for (eratype, erafile) in zip(ERA.eratypes, ERA.erafiles)
 
     lonlat = tuple.(lon, lat')
     longrid, latgrid = first.(lonlat), last.(lonlat)
-
-    snow_off_arr = snow_off(sd, time; min_snowy_days_before_snowpack_established = 30)
-
-    snow_off_mean = mapslices(x -> mean(filter(!isnan, x)), snow_off_arr; dims = 3)[:, :, 1]
+    years = nothing
 
     #Also plot basin averages
-    basin_snowoff_year = []
+    basin_snowoff = Dictionary()
     for basin in ERA.basin_names
         basinidxdir = joinpath(ERA.ERA5DATA, "basin_extractions", eratype)
         basin_idx_df = CSV.read(joinpath(basinidxdir, "$(basin)_era_points.csv"), DataFrame)
         basin_idxs = CartesianIndex.(basin_idx_df.lonidx, basin_idx_df.latidx)
-        basin_timeseries = snow_off_arr[basin_idxs, :]
-        basin_mean_snowoff =
-            vec(mapslices((x -> mean(filter(!isnan, x))), basin_timeseries; dims = (1,)))
-        push!(basin_snowoff_year, basin_mean_snowoff)
+        basin_sd_timeseries = sd[basin_idxs, :]
+        snow_offs = Vector{Union{Int, SnowOffType}}[]
+        for tseries in eachslice(basin_sd_timeseries, dims=1)
+            sd_df = DataFrame(datetime = Date.(time), sd = tseries; copycols = false)
+            with_year = transform!(sd_df, :datetime=>ByRow(year)=>:year)
+            grouped_year = groupby(with_year, :year)
+            snow_off_yearly = combine(grouped_year, 
+            [:sd, :datetime]=>((x...)->snow_off_single_year(x...))=>:snow_off)
+            push!(snow_offs, snow_off_yearly.snow_off)
+            isnothing(years) && (years = snow_off_yearly.year)
+        end
+        basinwide_data = VectorOfArray(snow_offs)
+        basin_mean_snow_off = vec(mapslices(x->mean(map(Float32, filter(x->!isa(x, SnowOffType), x))), basinwide_data, dims=2))
+        insert!(basin_snowoff, basin, basin_mean_snow_off)
     end
-    years = year(time[begin]):year(time[end])
-    basin_snowoff = reduce(hcat, basin_snowoff_year)
-    yticks =
-        round(minimum(filter(!isnan, basin_snowoff))):5:round(
-            maximum(filter(!isnan, basin_snowoff)),
-        )
-    yticklabel = Dates.format.((Date(2020) .+ Day.(yticks .- 1)), dateformat"mm/dd")
-    myp = plot(
-        years,
-        basin_snowoff;
-        label = permutedims(ERA.basin_names),
-        title = "ERA5-$eratype Snow Off By Year/Basin",
-        xlabel = "Year",
-        ylabel = "Basin Mean date of snow off (conversion from day of year to mm/dd ignores leap years, stations with no snow or permanent snow ignored)",
-        yticks = (yticks, yticklabel),
-        legend = :outertopright,
-        ylabelfontsize = 5,
-        size = 0.5e3 .* (2, 1),
-    )
+
+    minday, maxday = minimum(minimum.(ERA.skipnaormiss.(basin_snowoff))), maximum(maximum.(ERA.skipnaormiss.(basin_snowoff)))
+    daysofyear = minday-2:5:maxday+2
+    daylabels = Dates.format.(Date(2021) .+ Day.(round.(daysofyear) .- 1), dateformat"mm/dd")
+
+    myp = plot(;title="ERA5 $eratype Basin Mean Snow Off Dates", xlabel="year", ylabel="Mean date of snow off",
+                yticks = (daysofyear, daylabels))
+
+
+
+    for basin in ERA.basin_names
+        snow_off = basin_snowoff[basin]
+        plot!(myp, years, snow_off; label = "$basin")
+    end
 
     savefig(myp, "../vis/$(eratype)_basin_snow_off.png")
 end
--- Climatology/Snow_Off/src/snow_off_single_year.jl
+++ Climatology/Snow_Off/src/snow_off_single_year.jl
@@ -1,7 +1,10 @@
-#Threshold to be considered snowy (in m)
-const is_snow_thresh = 1e-3
+burrowactivate()
+import ERA5Analysis as ERA
 
-function snow_off_single_year(snow_bool, dates; min_snow)
+@enum SnowOffType::Int8 AlwaysSnow NeverSnow DoesntEstablish TooMuchMissing
+
+function snow_off_single_year(sd_data, dates; min_snowy_days = 30, is_snow_thresh = 2e-3, max_days_missing = 5)
+    snow_bool = sd_data .> is_snow_thresh
     diffs = snow_bool[2:end] .- snow_bool[1:(end - 1)]
     possible_snow_off_idxs = findall(==(-1), diffs)
     sort!(possible_snow_off_idxs)
@@ -9,24 +12,29 @@ function snow_off_single_year(snow_bool, dates; min_snow)
     #If there are no snow off idxs, determine if it's always or never snowy
     if isempty(possible_snow_off_idxs)
         if first(snow_bool) == 1
-            return NaN
+            return AlwaysSnow
         else
-            return NaN
+            return NeverSnow
         end
     end
 
-    #Now check if they fulfill the min_snow criterion
+    #Now check if they fulfill the min_snowy_days criterion
     #This iterates through the possible snow_off dates from earliest to latest as findall finds the earliest ones first
-    current_off = nothing
+    current_off = DoesntEstablish
     for idx in possible_snow_off_idxs
-        if idx < min_snow
+        if idx < min_snowy_days
             continue
-        elseif all(snow_bool[(idx - min_snow + 1):idx] .== 1)
+        end
+        snowy_before_tally = sum(ERA.skipnaormiss(snow_bool[(idx - min_snowy_days + 1):idx] .== 1))
+        n_miss_before_tally = count(ERA.na_or_miss, snow_bool[(idx - min_snowy_days + 1):idx])
+        if n_miss_before_tally > max_days_missing
+            current_off = TooMuchMissing
+        elseif snowy_before_tally >= (min_snowy_days - n_miss_before_tally)
             #It doesn't count if it happens after August
             month(dates[idx + 1]) >= 9 && continue
             current_off = idx
         end
     end
-    !isnothing(current_off) && return dayofyear(dates[current_off + 1])
-    return NaN
+    !isa(current_off, SnowOffType) && return dayofyear(dates[current_off + 1])
+    return current_off
 end
