1a2aea204e0ba051922a741e0bacfab510320f7a
Jesse <jesbrirob@gmail.com>
2022-08-01 18:33:33 -0800

    Added bias visualizations

--- BetterComparisons/SNOTEL/src/Peak_SWE/peak_eval.jl
+++ BetterComparisons/SNOTEL/src/Peak_SWE/peak_eval.jl
@@ -7,7 +7,7 @@ include("../../../Snow_Course/src/land_vs_base_func.jl")
 include("peak_datagen.jl")
 include(joinpath(ERA.COMPAREDIR, "Load Scripts", "load_era.jl"))
 
-dir = "../vis/plain_nn"
+dir = "../../vis/plain_nn"
 
 basin_to_snotel =
     jldopen(joinpath(ERA.NRCSDATA, "cleansed", "SNOTEL_basin_to_id.jld2"))["basin_to_id"]
@@ -19,32 +19,34 @@ mkpath(dir)
 
 snotel_basins = filter(x -> !isempty(basin_to_snotel[x]), ERA.usable_basins)
 
-for eratype in ERA.eratypes
-    stat_names = ["raw", "anom", "normed_anom", "fom"]
-    datavec = peak_datagen(;
-        eratype,
-        basin_to_stations = basin_to_snotel,
-        station_compare_args = peak_comp_args,
-        load_era_func = peak_swe_load(load_plain_nn),
-        stats_to_extract = ["raw", "anom", "normed_anom", "fom"] .* "_rmsd",
-        basins = snotel_basins
-    )
-    [data[isnan.(data)] .= 0 for data in datavec]
-    display(datavec)
-    style_kwargs = (;
-        title = "ERA5 $eratype vs SNOTEL Peak SWE",
-        ylabel = "Fraction of Median RMSD",
-        xlabel = "Water Year",
-        margin = 5Plots.mm,
-    )
-    error_bar_plot(
-        datavec,
-        dir;
-        plotname = "$(eratype)_peak_swe.png",
-        labels = ["Raw SWE", "Anomaly", "Normed Anomaly", "Frac. of Median"],
-        cvec = [:green, :blue, :purple, :red],
-        ylim = (0, 1.5),
-        xticklabels = snotel_basins,
-        style_kwargs
-    )
+for (stat, statname, ylim) in zip(("diff_mean", "rmsd"), ("Difference", "RMSD"), ((-0.1,0.1), (0,1.5)))
+    for eratype in ERA.eratypes
+        stat_names = ["raw", "anom", "normed_anom", "fom"]
+        datavec = peak_datagen(;
+            eratype,
+            basin_to_stations = basin_to_snotel,
+            station_compare_args = peak_comp_args,
+            load_era_func = peak_swe_load(load_plain_nn),
+            stats_to_extract = ["raw", "anom", "normed_anom", "fom"] .* "_" .* stat,
+            basins = snotel_basins
+        )
+        [data[isnan.(data)] .= 0 for data in datavec]
+        display(datavec)
+        style_kwargs = (;
+            title = "ERA5 $eratype vs SNOTEL Peak SWE",
+            ylabel = "Fraction of Median $statname",
+            xlabel = "Water Year",
+            margin = 5Plots.mm,
+        )
+        error_bar_plot(
+            datavec,
+            dir;
+            plotname = "$(eratype)_peak_swe_$stat.png",
+            labels = ["Raw SWE", "Anomaly", "Normed Anomaly", "Frac. of Median"],
+            cvec = [:green, :blue, :purple, :red],
+            ylim,
+            xticklabels = snotel_basins,
+            style_kwargs
+        )
+    end
 end
