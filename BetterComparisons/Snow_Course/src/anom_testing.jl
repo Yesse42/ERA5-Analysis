4051f4c921e93998a38a9317a5266bd4a1a6e259
Jesse <jesbrirob@gmail.com>
2022-08-02 17:44:57 -0800

    Added bias corrected rmsd

--- BetterComparisons/Snow_Course/src/anom_testing.jl
+++ BetterComparisons/Snow_Course/src/anom_testing.jl
@@ -13,28 +13,30 @@ func =
         load_era(joinpath(ERA.ERA5DATA, "better_extracted_points", "plain_nn"), eratype, id)
 
 mkpath(dir)
-for eratype in ERA.eratypes
-    stat_types = ["raw", "anom", "normed_anom", "fom"]
-    datavec = raw_anom_fom_comp_datagen(;
-        eratype,
-        load_era_func = load_plain_nn,
-        basin_to_stations = def_basin_to_station,
-        stats_to_extract = stat_types .* "_rmsd",
-    )
-    style_kwargs = (;
-        title = "$eratype Statistic Comparison, Naive NN, April 1st Snow Course",
-        titlefontsize = 12,
-        ylabel = "Fraction of Median RMSD",
-        xlabel = "Year",
-        margin = 5Plots.mm,
-    )
-    error_bar_plot(
-        datavec,
-        dir;
-        style_kwargs,
-        plotname = "$(eratype)_different_statistic_comparison.png",
-        labels = ["Raw SWE", "Anomaly", "Normed Anomaly", "Frac. of Median"],
-        cvec = [:green, :blue, :purple, :red],
-        ylim = (0, 1.5),
-    )
+for (stat, statname, bounds) in zip(("diff_mean", "rmsd", "bias_corrected_rmsd"), ("Bias", "RMSD", "Bias Corrected RMSD"), ((-0.2,0.2),(0, 1), (0,1)))
+    for eratype in ERA.eratypes
+        stat_types = ["raw", "anom", "normed_anom", "fom"]
+        datavec = raw_anom_fom_comp_datagen(;
+            eratype,
+            load_era_func = load_plain_nn,
+            basin_to_stations = def_basin_to_station,
+            stats_to_extract = stat_types .* "_$stat",
+        )
+        style_kwargs = (;
+            title = "$eratype Statistic Comparison, Naive NN, April 1st Snow Course",
+            titlefontsize = 12,
+            ylabel = "Fraction of Median $statname",
+            xlabel = "Year",
+            margin = 5Plots.mm,
+        )
+        error_bar_plot(
+            datavec,
+            dir;
+            style_kwargs,
+            plotname = "$(eratype)_$(statname)_different_statistic_comparison.png",
+            labels = ["Raw SWE", "Anomaly", "Normed Anomaly", "Frac. of Median"],
+            cvec = [:green, :blue, :purple, :red],
+            ylim = bounds,
+        )
+    end
 end
